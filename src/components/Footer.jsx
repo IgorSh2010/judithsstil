4f28c1e08dbf42a298286a24f47a017bf29cc6e2
IgorSh2010 <Igorsh2010@gmail.com>
2025-10-26 16:45:58 +0100

    Updated from VS Studio code - $(date '+m-H:M')

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
     <footer className="bg-gray-900 text-gray-200">
       <div className="max-w-7xl mx-auto px-4 py-4 grid md:grid-cols-4 gap-6">
         <div>
-            <img src="/logo3.png" alt="Logo" className="h-25" />
+            <img src="/logo2.png" alt="Logo" className="h-25" />
         </div>
         <div>            
           <h4 className="font-semibold mb-2">Judiths stil</h4>
