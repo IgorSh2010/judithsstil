56c25c314ffe59363accde55a1195c8dab15853e
IgorSh2010 <Igorsh2010@gmail.com>
2025-10-26 12:19:26 +0100

    Updated from VS Studio code - $(date '+m-H:M')

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "https://129.159.28.206/api";
+//const API_URL = "https://129.159.28.206/api";
+const API_URL = "/api";
 
 const api = axios.create({
   baseURL: API_URL,
